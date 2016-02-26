4d72602cffe09f5e6185abe1640cdc4b6ecc4ba1
Joe Attardi <joe@attardi.net>
2016-02-26 00:07:27 -0500

    back to the drawing board

--- src/spec/promise-poller-spec.js
+++ src/spec/promise-poller-spec.js
@@ -1,35 +1,65 @@
-import PromisePoller from '../lib/promise-poller';
+import promisePoller from '../lib/promise-poller';
 import Promise from 'bluebird';
 
 describe('Promise Poller', function() {
-  describe('#start', function() {
-    it('returns a master promise', function() {
-      let poller = new PromisePoller(() => {Promise.resolve('yay')}, 1000, 5);
-      let result = poller.start();
-      expect(typeof result.then).toBe('function');
-      result.then(() => {}, () => {});
+  it('returns a promise', function() {
+    const fn = () => Promise.resolve('yay');
+    const poller = promisePoller(1000, 5, fn);
+    expect(poller.then).toBeDefined();
+    expect(typeof poller.then).toBe('function');
+  });
+
+  it('resolves the master promise when the poll succeeds', function(done) {
+    const fn = () => Promise.resolve('yay');
+    promisePoller(1000, 5, fn).then((result) => {
+      expect(result).toBe('yay');
+      done();
+    }, () => fail('Master promise was rejected'));
+  });
+
+  it('rejects the master promise when the poll fails', function(done) {
+    const fn = () => Promise.reject('derp');
+    promisePoller(500, 3, fn).then(() => fail('Master promise was resolved'), 
+      (err) => {
+        expect(err).toBe('derp');
+        done();
     });
   });
 
-  it('resolves the master promise if the task succeeds', function(done) {
-    let taskFn = function() {
-      return Promise.resolve('yay');
+  it('waits the given interval between attempts', function(done) {
+    let last = 0; 
+    let now;
+    const fn = () => {
+      now = Date.now();
+      if (last) {
+        expect(now - last).toBeGreaterThan(500); 
+      }
+      last = now;
+      return Promise.reject('derp');
+    };
+    
+    promisePoller(500, 3, fn).then(null, done);
+  });
+
+  it('tries <retries> times before giving up', function(done) {
+    let counter = 0;
+    const fn = () => {
+      counter++;
+      return Promise.reject('derp');
     };
 
-    let poller = new PromisePoller(taskFn, 1000, 5);
-    poller.start().then(done, function() {
-      fail('Master promise was rejected');
+    promisePoller(500, 3, fn).then(null, () => {
+      expect(counter).toBe(3);
+      done();
     });
   });
 
-  it('rejects the master promise if the task fails', function(done) {
-    let taskFn = function() {
-      return Promise.reject('error');      
-    };
-
-    let poller = new PromisePoller(taskFn, 1000, 5);
-    poller.start().then(function() {
-      fail('Master promise was resolved');
-    }, done);
+  it('calls the progress callback with each failure', function(done) {
+    const fn = () => Promise.reject('derp');
+    let callback = jasmine.createSpy();
+    promisePoller(500, 3, fn, callback).then(null, (err) => {
+      expect(callback.calls.count()).toBe(3);
+      done();
+    });
   });
 });
