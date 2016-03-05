e78ddb9f61259ff5eeedc8693359adf66300490a
Joe Attardi <joe@attardi.net>
2016-03-05 13:08:39 -0500

    Add different strategies

--- src/lib/promise-poller.js
+++ src/lib/promise-poller.js
@@ -3,21 +3,66 @@ import debugModule from 'debug';
 
 const debug = debugModule('promisePoller');
 
+const strategies = {
+  'fixed-interval': {
+    defaults: {
+      interval: 1000
+    },
+    getNextInterval: function(count, options) {
+      return options.interval;
+    }
+  },
+
+  'linear-backoff': {
+    defaults: {
+      start: 1000,
+      increment: 1000
+    },
+    getNextInterval: function(count, options) {
+      return options.start + (options.increment * (count - 1));
+    }
+  },
+
+  'exponential-backoff': {
+    defaults: {
+      min: 1000,
+      max: 30000
+    },
+    getNextInterval: function(count, options) {
+      const rand = Math.round(Math.random() * (Math.pow(2, count) * 1000 - options.min));
+      return Math.min(options.max, rand);
+    }
+  }
+};
+
 const DEFAULTS = {
-  interval: 1000,
+  strategy: 'fixed-interval',
   retries: 5
 };
 
 let pollerCount = 0;
 
 function promisePoller(options = {}) {
+  if (typeof options.taskFn !== 'function') {
+    throw new Error('No taskFn function specified in options');
+  }
+
   Object.keys(DEFAULTS).forEach(option => options[option] = options[option] || DEFAULTS[option]);
   options.name = options.name || `Poller-${pollerCount++}`;
   debug(`Creating a promise poller "${options.name}" with interval=${options.interval}, retries=${options.retries}`);
 
-  if (typeof options.taskFn !== 'function') {
-    throw new Error('No taskFn function specified in options');
+  if (!strategies[options.strategy]) {
+    throw new Error(`Invalid strategy "${options.strategy}". Valid strategies are ${Object.keys(strategies)}`);
   }
+  const strategy = strategies[options.strategy];
+  debug(`(${options.name}) Using strategy "${options.strategy}".`);
+  const strategyDefaults = strategy.defaults;
+  Object.keys(strategyDefaults).forEach(option => options[option] = options[option] || strategyDefaults[option]);
+
+  debug(`(${options.name}) Options:`);
+  Object.keys(options).forEach(option => {
+    debug(`    "${option}": ${options[option]}`);
+  });
 
   return new Promise(function(resolve, reject) {
     let retriesRemaining = options.retries;
@@ -35,8 +80,11 @@ function promisePoller(options = {}) {
           reject(err);
         } else {
           debug(`(${options.name}) Poll failed. ${retriesRemaining} retries remaining.`);
-          debug(`(${options.name}) Waiting ${options.interval}ms to try again.`);
-          Promise.delay(options.interval).then(poll);
+
+          const nextInterval = strategy.getNextInterval(options.retries - retriesRemaining, options);
+
+          debug(`(${options.name}) Waiting ${nextInterval}ms to try again.`);
+          Promise.delay(nextInterval).then(poll);
         }
       });
     }
